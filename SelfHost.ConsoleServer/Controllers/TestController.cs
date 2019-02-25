9ad56c55cfa09175f888d22c1953bd5a499f50ff
Mridul Saurabh Verma <saurabh.mridul@outlook.com>
2019-02-25 21:28:32 +0530

    adding routes

--- SelfHost.ConsoleServer/Controllers/TestController.cs
+++ SelfHost.ConsoleServer/Controllers/TestController.cs
@@ -5,9 +5,29 @@ namespace SelfHost.ConsoleServer.Controllers
 {
     public class TestController : ApiController
     {
-        public IEnumerable<string> GetTests()
+        [HttpGet]
+        public IEnumerable<Test> GetTests()
         {
-            return new string[] { "Hello", "World" };
+            return new List<Test> {
+                new Test{ id=1, title="home", homeAddress="bangalore"},
+                new Test{ id=2, title="home1", homeAddress="bangalore"},
+                new Test{ id=3, title="home2", homeAddress="bangalore"},
+                new Test{ id=4, title="home3", homeAddress="bangalore"}
+            };
         }
+
+        [HttpGet]
+        public Test SetTest()
+        {
+            return new Test() { id = 10, title = "there there", homeAddress = "home" };
+        }
+    }
+
+
+    public class Test
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string homeAddress { get; set; }
     }
 }
